75733a51d43664c6967ab13b4f4e86162f4e5204
shibihh <shibilka05@gmail.com>
2025-05-09 01:46:57 +0530

    Update SetupBanners.js

--- UI/banners/SetupBanners.js
+++ UI/banners/SetupBanners.js
@@ -1,5 +1,5 @@
 const setupBanners = {
-    helpBanner : 'https://i.ibb.co/23C0zG31/standard-1.gif',
+    helpBanner : 'https://cdn.discordapp.com/attachments/1361444008764768509/1363512746989195545/standard.gif?ex=68064db3&is=6804fc33&hm=d5c607f1777d4f7e74cc5e90187c2d2e9639d32a5dc9b964241c4f8fbe87f111&',
     verificationBanner : 'https://i.ibb.co/pjHDXzXx/static.png',
     ticketBanner : 'https://i.ibb.co/h1DBsRnv/ticket.gif',
     autovcBanner : 'https://i.ibb.co/mk6Tj1r/autovc.gif',
