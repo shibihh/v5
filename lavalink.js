932c0d2387cc6208234cf5e054dc65782dd213e6
shibihh <shibilka05@gmail.com>
2025-05-09 02:03:53 +0530

    Update lavalink.js

--- lavalink.js
+++ lavalink.js
@@ -2,10 +2,10 @@
 module.exports = {
     enabled: true, 
     lavalink: {
-        name: "GlaceYT",
-        password: "glaceyt",
-        host: "193.226.78.187",
-        port:  3543,
-        secure: false
+           name: "dj wale babu",
+      password: "inosuke",
+      host: "inosuke.69.mu",
+      port:  11622,
+      secure: false
     }
 };
